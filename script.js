c297cbe5a407e8aeb533fe6700e3f674464d2183
Saad Aulakh <saadaulakh04@gmail.com>
2023-10-01 13:51:55 -0400

    Integrated emailjs and made the design responsive

--- script.js
+++ script.js
@@ -3,3 +3,30 @@ const header = document.querySelector("header");
 window.addEventListener("scroll", function () {
     header.classList.toggle("sticky", window.scrollY > 120)
 })
+
+let menu = document.querySelector('#menu-icon')
+let navlist = document.querySelector('.navlist')
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x')
+    navlist.classList.toggle('active')
+}
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x')
+    navlist.classList.remove('active')
+}
+
+function SendMail() {
+    var params = {
+        from_name : document.getElementById("fullName").value,
+        email_id : document.getElementById("email_id").value,
+        message : document.getElementById("message").value,
+    }
+    emailjs.send("service_xjcey7p", "template_6mmcpga", params).then(function (res) {
+        alert("Thank you for sending a message!");
+        document.getElementById("fullName").value = ""
+        document.getElementById("email_id").value =""
+        document.getElementById("message").value =""
+    })
+}
